73afc8187fb6de641e1835789c3cc8fca7b7d7c5
dhruvpathak9305 <dhruvpathak9305@gmail.com>
2020-01-22 02:50:15 +0530

    guess fn and window addEventListener added.

--- hangman-Dumshrades-app/hangman.js
+++ hangman-Dumshrades-app/hangman.js
@@ -1,7 +1,12 @@
+// Create a method for making a guess
+// 1. Should accept a character for guessing
+// 2. Should add unique guesses to list of guesses
+// 3. Should decrement the guesses left if a unique guess isn't a match
+
 const Hangman = function (word, remainingGuesses) {
     this.word = word.toLowerCase().split('')
     this.remainingGuesses = remainingGuesses
-    this.guessedLetters = ['c', 'e']
+    this.guessedLetters = []
 }
 
 Hangman.prototype.getPuzzle = function () {
@@ -18,8 +23,28 @@ Hangman.prototype.getPuzzle = function () {
     return puzzle
 }
 
-const game1 = new Hangman('Cat', 2)
-console.log(game1.getPuzzle())
+Hangman.prototype.makeGuess = function (guess) {
+    guess = guess.toLowerCase()
+    const isUnique = !this.guessedLetters.includes(guess)
+    const isBadGuess = !this.word.includes(guess)
 
-const game2 = new Hangman('New Jersey', 4)
-console.log(game2.getPuzzle())
+    if (isUnique) {
+        this.guessedLetters.push(guess)
+    }
+
+    if (isUnique && isBadGuess) {
+        this.remainingGuesses--
+    }
+}
+
+const game1 = new Hangman('Cat', 2)
+
+console.log(game1.getPuzzle())
+console.log(game1.remainingGuesses)
+
+window.addEventListener('keypress', function (e) {
+    const guess = String.fromCharCode(e.charCode)
+    game1.makeGuess(guess)
+    console.log(game1.getPuzzle())
+    console.log(game1.remainingGuesses)
+})
