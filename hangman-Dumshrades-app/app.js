a1dab91dc0b2cff9beb2f20a1245c0036807b884
dhruvpathak9305 <dhruvpathak9305@gmail.com>
2020-01-22 03:35:18 +0530

    HTTP Requests from JavaScript

--- hangman-Dumshrades-app/app.js
+++ hangman-Dumshrades-app/app.js
@@ -1,3 +1,6 @@
+// HTTP (Hypertext Transfer Protocol)
+// Request - What do we want to do
+// Response - What was actually done
 const puzzleEl = document.querySelector('#puzzle')
 const guessesEl = document.querySelector('#guesses')
 const game1 = new Hangman('Car Parts', 2)
@@ -11,3 +14,16 @@ window.addEventListener('keypress', function (e) {
     puzzleEl.textContent = game1.puzzle
     guessesEl.textContent = game1.statusMessage
 })
+
+// Making an HTTP request
+const request = new XMLHttpRequest()
+
+request.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4) {
+        const data = JSON.parse(e.target.responseText)
+        console.log(data)
+    }
+})
+
+request.open('GET', 'http://puzzle.mead.io/puzzle')
+request.send()
