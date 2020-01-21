7e463f401db8b0526ef51f8ba6c2870f08a26873
dhruvpathak9305 <dhruvpathak9305@gmail.com>
2020-01-22 04:47:34 +0530

    Promise part 2

--- hangman-Dumshrades-app/app.js
+++ hangman-Dumshrades-app/app.js
@@ -18,20 +18,10 @@ getPuzzle('2').then((puzzle) => {
     console.log(`Error: ${err}`)
 })
 
-// getCountry('MX').then((country) => {
-//     console.log(country.name)
-// }, (err) => {
-//     console.log(`Error: ${err}`)
-// })
-
-// fetch('http://puzzle.mead.io/puzzle', {}).then((response) => {
-//     if (response.status === 200) {
-//         return response.json() 
-//     } else {
-//         throw new Error('Unable to fetch the puzzle')
-//     }
-// }).then((data) => {
-//     console.log(data.puzzle)
-// }).catch((error) => {
-//     console.log(error)
-// })
+getLocation().then((location) => {
+    return getCountry(location.country)
+}).then((country) => {
+    console.log(country.name)
+}).catch((err) => {
+    console.log(`Error: ${err}`)
+})
--- hangman-Dumshrades-app/requests.js
+++ hangman-Dumshrades-app/requests.js
@@ -10,19 +10,24 @@ const getPuzzle = (wordCount) => {
     })
 }
 
-const getCountry = (countryCode) => new Promise((resolve, reject) => {
-    const countryRequest = new XMLHttpRequest()
+const getCountry = (countryCode) => {
+    return fetch('http://restcountries.eu/rest/v2/all').then((response) => {
+        if (response.status === 200) {
+            return response.json()
+        } else {
+            throw new Error('Unable to fetch data')
+        }
+    }).then((data) => {
+        return data.find((country) => country.alpha2Code === countryCode)
+    })
+}
 
-    countryRequest.addEventListener('readystatechange', (e) => {
-        if (e.target.readyState === 4 && e.target.status === 200) {
-            const data = JSON.parse(e.target.responseText)
-            const country = data.find((country) => country.alpha2Code === countryCode)
-            resolve(country)
-        } else if (e.target.readyStatet === 4) {
-            reject('Unable to fetch data')
+const getLocation = () => {
+    return fetch('http://ipinfo.io/json?token=1a11bd55cc8f9c').then((response) => {
+        if (response.status === 200) {
+            return response.json()
+        } else {
+            throw new Error('Unable to fetch location')
         }
     })
-
-    countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all')
-    countryRequest.send()
-})
+}
